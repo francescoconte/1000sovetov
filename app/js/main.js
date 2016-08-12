db54d6fdd0d9102a244ddfbcc04daf923792adea
Александр <francesco-lucania@be-simple.su>
2016-08-12 17:47:47 +0300

    yap

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+		$(document).ready(function() {
+			$('#fullpage').fullpage({
+				anchors: ['start-info', 'about-info', 'service-info', 'actual-info', 'contact-info'],
+				menu: '#menu',
+				scrollingSpeed: 1000
+			});
+
+		});
+
